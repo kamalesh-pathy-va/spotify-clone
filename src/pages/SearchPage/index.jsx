3dc80039d5251a9e6cd230b5d2c5c79c7aa0a4bd
kamalesh-pathy-va <asokankamalesh@gmail.com>
2023-09-08 13:43:25 +0530

    search page relocated

---
+++ src/pages/SearchPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Topnav } from '../../components/Topnav'
+
+export const SearchPage = () => {
+  return (
+    <>
+      <Topnav search/>
+      <h1>SearchPage</h1>
+    </>
+  )
+}
